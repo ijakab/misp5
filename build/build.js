fc57c3cfc06fa06ee2a51d84625c3d92ac57f4e0
Ivan Jakab <ivan.jakab@gauss.hr>
2019-05-24 18:39:12 +0200

    initial commit

---
+++ build/build.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+function draw() {
+    background(100);
+}
+//# sourceMappingURL=build.js.map
