9a8c5ce06dbe357e65acfe769e044addb2649a52
Ivan Jakab <ivan.jakab@gauss.hr>
2019-06-01 12:36:14 +0200

    rotating logic to vector class

--- sketch/Vector.ts
+++ sketch/Vector.ts
@@ -2,11 +2,11 @@ class Vector {
     constructor(public i: number, public j: number) {
     }
 
-    public amount() {
+    public amount(): number {
         return Math.sqrt(this.i*this.i + this.j*this.j)
     }
 
-    public angle() {
+    public angle(): number {
         return Math.atan(this.j/this.i)
     }
 
@@ -14,6 +14,24 @@ class Vector {
         let ratio: number = newAmount / this.amount();
         this.i *= ratio;
         this.j *= ratio;
+        return this;
+    }
+
+    public setAngle(angle: number): Vector {
+        let oldAmount = this.amount();
+        this.i = 1; //arbitrary
+        this.j = Math.tan(angle); //j = i*tan(angle), and i = 1
+        this.setAmount(oldAmount); //we don't change amount
+        return this;
+    }
+
+    public rotateRight(angle: number): Vector {
+        this.setAngle(this.angle() + angle);
+        return this;
+    }
+
+    public rotateLeft(angle: number): Vector {
+        this.setAngle(this.angle() + angle);
         return this;
     }
 }
